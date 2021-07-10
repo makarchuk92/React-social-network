8a759eca61ed17d01adc0ef80abc9649b9d01d1e
Andrei <andrei.makarchuk@mail.ru>
2021-07-11 00:03:05 +0300

    Added autoScroll in ChatPage

--- src/Components/ChatPage/ChatPage.tsx
+++ src/Components/ChatPage/ChatPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { HtmlHTMLAttributes, useEffect, useRef, useState } from "react"
 import { Button } from "antd"
 import { ChatMessageType } from "../../api/chat-api"
 import { useDispatch, useSelector } from "react-redux"
@@ -10,10 +10,10 @@ import { AppStateType } from "../../redux/redux-store"
 
 
 const ChatPage: React.FC = () => {
-    return  <div>
-                <Chat />
-            </div>
-    
+    return <div>
+        <Chat />
+    </div>
+
 }
 
 const Chat: React.FC = () => {
@@ -32,21 +32,40 @@ const Chat: React.FC = () => {
 
 
 
-    return  <div>
-            {status === 'error' && <div>Please, refresh the page</div>}
-                <>
-                    <Messages />
-                    <AddMessageChatForm />
-                </>  
-        </div>
+    return <div>
+        {status === 'error' && <div>Please, refresh the page</div>}
+        <>
+            <Messages />
+            <AddMessageChatForm />
+        </>
+    </div>
 }
 
 const Messages: React.FC<{}> = () => {
     const messages = useSelector((state: AppStateType) => state.chat.messages)
+    const messagesAnchorRef = useRef<HTMLDivElement>(null)
+    const [isAutoScroll, setIsAutoScroll] = useState(false)
+
+    const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const element = e.currentTarget
+        if(Math.abs( (element.scrollHeight - element.scrollTop) - element.clientHeight) < 300 ) {
+            !isAutoScroll && setIsAutoScroll(true)
+        }else{
+            isAutoScroll && setIsAutoScroll(false)
+        }
+    }
+
+    useEffect(() => {
+        if(isAutoScroll) {
+            messagesAnchorRef.current?.scrollIntoView({ behavior: 'auto' })
+        }
+    }, [messages])
+
 
     return (
-        <div style={{ height: "400px", overflowY: "auto" }}>
+        <div style={{ height: "400px", overflowY: "auto" }} onScroll={scrollHandler} >
             {messages.map((m, index) => <Message message={m} key={index} />)}
+            <div ref={messagesAnchorRef}></div>
         </div>
     )
 }
@@ -77,13 +96,13 @@ const AddMessageChatForm: React.FC<{}> = () => {
         setMessage('')
     }
 
-    return ( 
+    return (
         <div>
             <div>
                 <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}></textarea>
             </div>
             <div>
-                <Button  disabled={status !== 'ready'} onClick={sendMessageHamdler} type="primary">
+                <Button disabled={status !== 'ready'} onClick={sendMessageHamdler} type="primary">
                     send</Button>
             </div>
         </div>
